a3c44f4ae0447e0c370bba11afcea0241461a030
dpanza <david.panza@gmail.com>
2016-05-29 19:42:32 +0200

    init npm & init simple test

---
+++ test/gilded_rose.spec.js
@@ -0,0 +1,11 @@
+var chai = require('chai');
+var expect = chai.expect;
+var GilderRose = require('../src/gilded_rose.js')["default"].GildedRose;
+
+describe("Gilded Rose", function() {
+
+  it("should do something", function() {
+    GilderRose.updateQuality();
+  });
+
+});
